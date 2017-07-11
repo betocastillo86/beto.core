71fa4665245e75383a5bfcdce81c019965127b3f
Gabriel <gabriel.castillo86@hotmail.com>
2017-07-11 11:01:25 -0500

    Se agrega logger service y ExceptionsMiddleware

---
+++ src/Beto.Core/Exceptions/ILoggerService.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ILoggerService.cs" company="Gabriel Castillo">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Beto.Core.Exceptions
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interface of general logger service. It Has to have an implementation with the project that used it.
+    /// </summary>
+    public interface ILoggerService
+    {
+        /// <summary>
+        /// Inserts the specified short message.
+        /// </summary>
+        /// <param name="shortMessage">The short message.</param>
+        /// <param name="fullMessage">The full message.</param>
+        void Insert(string shortMessage, string fullMessage = "");
+
+        /// <summary>
+        /// Inserts the asynchronous.
+        /// </summary>
+        /// <param name="shortMessage">The short message.</param>
+        /// <param name="fullMessage">The full message.</param>
+        /// <returns>the task</returns>
+        Task InsertAsync(string shortMessage, string fullMessage = "");
+    }
+}
