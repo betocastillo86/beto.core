5ad7a49e9562441a7fa3b355468c8c9acd5e740f
Gabriel <gabriel.castillo86@hotmail.com>
2017-07-12 16:15:25 -0500

    Se agrega interfaz de creacion de Token de autenticacion

---
+++ src/Beto.Core.Web/Security/GeneratedAuthenticationToken.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ExceptionsMiddleware.cs" company="Gabriel Castillo">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Beto.Core.Web.Security
+{
+    /// <summary>
+    /// The token generated after authentication
+    /// </summary>
+    public class GeneratedAuthenticationToken
+    {
+        /// <summary>
+        /// Gets or sets the access token.
+        /// </summary>
+        /// <value>
+        /// The access token.
+        /// </value>
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expires.
+        /// </summary>
+        /// <value>
+        /// The expires.
+        /// </value>
+        public int Expires { get; set; }
+    }
+}
---
+++ src/Beto.Core.Web/Security/IAuthenticationTokenGenerator.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IAuthenticationTokenGenerator.cs" company="Gabriel Castillo">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Beto.Core.Web.Security
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using System.Security.Principal;
+
+    /// <summary>
+    /// Interface for generating a token for authentication
+    /// </summary>
+    public interface IAuthenticationTokenGenerator
+    {
+        /// <summary>
+        /// Generates the token for authenticate a user
+        /// </summary>
+        /// <param name="genericIdentity">The generic identity.</param>
+        /// <param name="claims">The claims.</param>
+        /// <param name="generationDate">The date when the key is generation. Usually you can use DateTimeOffset.Now</param>
+        /// <param name="configParams">configuration parameters depending of type of authentication</param>
+        /// <returns>The generated token for authentication</returns>
+        GeneratedAuthenticationToken GenerateToken(GenericIdentity genericIdentity, IList<Claim> claims, DateTimeOffset generationDate, IDictionary<string, string> configParams);
+    }
+}
