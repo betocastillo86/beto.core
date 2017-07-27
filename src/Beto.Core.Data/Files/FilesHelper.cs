769f7c5e321e50935586db92f767ab34c762e52b
Gabriel <gabriel.castillo86@hotmail.com>
2017-07-27 18:50:40 -0500

    Ajuste resize para save image

--- src/Beto.Core.Data/Files/FilesHelper.cs
+++ src/Beto.Core.Data/Files/FilesHelper.cs
@@ -203,7 +203,7 @@ namespace Beto.Core.Data.Files
             if (this.IsImageExtension(fullPath))
             {
                 // Resizes the image with the same name
-                this.pictureResizerService.ResizePicture(bytes, fullPath, resizeWidth, resizeHeight);
+                this.pictureResizerService.ResizePicture(bytes, fullPath, resizeWidth, resizeHeight, ResizeMode.Pad);
             }
             else
             {
