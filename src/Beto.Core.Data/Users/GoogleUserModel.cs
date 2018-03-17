a84a48140efb8b10bdee3e9b01e288672ca5ff82
Gabriel Castillo <gabriel.castillo86@hotmail.com>
2018-03-17 18:51:49 -0500

    Se agrega google model

---
+++ src/Beto.Core.Data/Users/GoogleUserModel.cs
@@ -0,0 +1,53 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GoogleUserModel.cs" company="Gabriel Castillo">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Beto.Core.Data.Users
+{
+    /// <summary>
+    /// Google User Model
+    /// </summary>
+    public class GoogleUserModel
+    {
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error.
+        /// </summary>
+        /// <value>
+        /// The error.
+        /// </value>
+        public string Error { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the picture.
+        /// </summary>
+        /// <value>
+        /// The picture.
+        /// </value>
+        public string Picture { get; set; }
+    }
+}
