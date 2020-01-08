d40298718eabe79a988c84465fe210b28964d8d2
Gabriel Castillo <Gabriel.Castillo@co.ey.com>
2020-01-08 10:28:53 -0500

    Added BulkInsert method

--- src/Beto.Core/Data/IDbContext.cs
+++ src/Beto.Core/Data/IDbContext.cs
@@ -1,55 +1,25 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="IDbContext.cs" company="Dasigno">
-//     Company copyright tag.
-// </copyright>
-//-----------------------------------------------------------------------
-namespace Beto.Core.Data
+﻿namespace Beto.Core.Data
 {
     using System;
+    using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.ChangeTracking;
     using Microsoft.EntityFrameworkCore.Infrastructure;
 
-    /// <summary>
-    /// Interface for Entity Framework Context
-    /// </summary>
     public interface IDbContext : IDisposable
     {
-        /// <summary>
-        /// Gets the database.
-        /// </summary>
-        /// <value>
-        /// The database.
-        /// </value>
         DatabaseFacade Database { get; }
 
-        /// <summary>
-        /// Saves the changes.
-        /// </summary>
-        /// <returns>modified records</returns>
         int SaveChanges();
 
-        /// <summary>
-        /// Saves Changes asynchronous
-        /// </summary>
-        /// <param name="cancellationToken">the cancellation token</param>
-        /// <returns>the task</returns>
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
 
-        /// <summary>
-        /// Sets this instance.
-        /// </summary>
-        /// <typeparam name="TEntity">The type of the entity.</typeparam>
-        /// <returns>the table</returns>
         DbSet<TEntity> Set<TEntity>() where TEntity : class;
 
-        /// <summary>
-        /// Entries the specified entity.
-        /// </summary>
-        /// <param name="entity">The entity.</param>
-        /// <returns>the entry</returns>
         EntityEntry Entry(object entity);
+
+        void BulkInsert<T>(IList<T> entities, BulkConfigCore bulkConfig = null, Action<decimal> progress = null) where T : class;
     }
 }
