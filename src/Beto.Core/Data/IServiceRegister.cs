8236d4d77edec5f5b3f3f78ae833aecc5d93a00d
betocastillo86 <castillopradagabriel@gmail.com>
2020-07-01 17:16:57 -0500

    Added IServiceRegister

---
+++ src/Beto.Core/Data/IServiceRegister.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Beto.Core.Registers
+{
+    public interface IServiceRegister
+    {
+        void Register(IServiceCollection services, IConfigurationRoot configuration);
+    }
+}
