dadf53298b67fd4663bbc51e04f8ee2be94de582
betocastillo86 <castillopradagabriel@gmail.com>
2021-03-16 19:53:33 -0500

    Added keyvault on register

--- src/Beto.Core/Data/IServiceRegister.cs
+++ src/Beto.Core/Data/IServiceRegister.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Beto.Core.Security;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Beto.Core.Registers
 {
     public interface IServiceRegister
     {
-        void Register(IServiceCollection services, IConfigurationRoot configuration);
+        void Register(IServiceCollection services, IConfigurationRoot configuration, IKeyVaultService keyVaultService);
     }
 }
